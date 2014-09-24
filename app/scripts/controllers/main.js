41010f040139617670224a99e07149fea5a87965
ayf media <asael2@gmail.com>
2014-09-23 23:24:31 -0500

    searching term

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,17 @@
  * # MainCtrl
  * Controller of the thepelisApp
  */
-angular.module('thepelisApp').controller('MainCtrl', function ($scope) {
+angular.module('thepelisApp').controller('MainCtrl', function ($scope, $http) {
 
-	
+	var apiUrl = 'http://api.themoviedb.org/3/',
+		mode = 'search/multi',
+		apiKey = '?api_key=7d01deb61b99198006a40696c46d3f29&query=';
 
-
+    $scope.fetchData = function() {
+    	$http.get(apiUrl+mode+apiKey+$scope.searchTerm).success(function(data){
+    		console.log(data);
+    		$scope.pelis = data.results;
+    	})
+       
+    }
 });
